02e4ac591a8d60b1fd0ac4e79c4053d3ff2be3a1
Andreas Fritzler <andreas.fritzler@gmail.com>
2018-01-12 22:18:55 +0100

    Fixed startup and added gitignore

--- bin/mine.sh
+++ bin/mine.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+echo "Starting up ..."
+echo "docker run -it afritzler/cpuminer-arm:v1 /minerd -a $ALGO -o $URL -u $EMAIL -p x -t $THREADS"
+
 docker run -it afritzler/cpuminer-arm:v1 /minerd -a $ALGO -o $URL -u $EMAIL -p x -t $THREADS
